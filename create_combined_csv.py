ea6029e20cf9e1a3b110f88f9a7f7f6f63c2c3de
disfear <disfear86@gmail.com>
2017-06-02 00:58:38 +0300

    Update create_combined_csv.py

--- create_combined_csv.py
+++ create_combined_csv.py
@@ -7,13 +7,5 @@ ratings = pd.read_csv('data/u.data', sep='\t',
 m_cols = ['movie_id', 'title']
 movies = pd.read_csv('data/u.item', sep='|', names=m_cols, usecols=range(2))
 
-movie_ratings = pd.merge(ratings, movies)
+movie_ratings = pd.merge(movies, ratings)
 movie_ratings.to_csv('app/movie_ratings.csv')
-movie_ratings = movie_ratings.pivot_table(index=['user_id'],
-                                            columns=['movie_id'],
-                                            values=['rating'])
-
-
-# restack and write to file
-r = movie_ratings.stack()
-r.to_csv('data/ratings.csv')
