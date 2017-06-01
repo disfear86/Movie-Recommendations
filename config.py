7c156b49bb4eded2d7dc6c43feb58baf852f39de
disfear <disfear86@gmail.com>
2017-06-02 00:59:35 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,3 +6,5 @@ SQLALCHEMY_DATABASE_URI = 'mysql://<user>:<password>@localhost/<database name>'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = 'dev-key'
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
