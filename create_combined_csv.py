1f71747c5d7efc973c2af80aae53b533174a2486
Kalliadis Papaiakovou <disfear86@gmail.com>
2017-06-01 12:19:30 +0300

    Update csv path

--- create_combined_csv.py
+++ create_combined_csv.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
 r_cols = ['user_id', 'movie_id', 'rating']
-ratings = pd.read_csv('app/data/u.data', sep='\t',
+ratings = pd.read_csv('data/u.data', sep='\t',
                         names=r_cols, usecols=range(3))
 
 m_cols = ['movie_id', 'title']
-movies = pd.read_csv('app/data/u.item', sep='|', names=m_cols, usecols=range(2))
+movies = pd.read_csv('data/u.item', sep='|', names=m_cols, usecols=range(2))
 
 movie_ratings = pd.merge(ratings, movies)
 movie_ratings = movie_ratings.pivot_table(index=['user_id'],
